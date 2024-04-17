f9952800f0dc74b2cfa8fabc623f80feaf27de5a
Sebastien Escudero <sebastien.escudero@gmail.com>
2024-04-17 16:57:53 +0200

    issue #3 & #4 : error message & confirmation screen ok !

--- scripts/modal.js
+++ scripts/modal.js
@@ -11,12 +11,17 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelector(".close");
+const closeBtnSuccessModal = document.querySelector(".btn-successModal");
 const content = document.querySelector(".content");
+const successModal = document.querySelector('.successModal');
+const form = document.getElementById('form');
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 closeBtn.addEventListener("click", closeModal);
+closeBtnSuccessModal.addEventListener('click', closeModal);
 
 // Launch modal form
 export function launchModal() {
@@ -25,9 +30,14 @@ export function launchModal() {
 // Close modal form
 export function closeModal() {
   content.classList.toggle("--hidden");
+  successModal.classList.toggle("--hidden");
   setTimeout(function() {
     modalbg.style.display = "none";
   }, 500); 
 }
-
+// On validation success, display success modal
+export function showSuccessModal() {
+  form.style.display = "none";
+  successModal.style.display = "flex";
+}
 
