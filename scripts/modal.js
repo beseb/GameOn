89ecb05724af2e3fdf6f6c36ae0028327881ae95
Sebastien Escudero <sebastien.escudero@gmail.com>
2024-04-17 14:53:04 +0200

    Issue #1 : button close modal ok

--- scripts/modal.js
+++ scripts/modal.js
@@ -11,13 +11,23 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const content = document.querySelector(".content");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+// close modal event
+closeBtn.addEventListener("click", closeModal);
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+// Close modal form
+function closeModal() {
+  content.classList.toggle("--hidden");
+  setTimeout(function() {
+    modalbg.style.display = "none";
+  }, 500); 
 }
 
 
